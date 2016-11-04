bb950dc4dd900dfa47af5e96cac24a3c51fa2b21
Бугримов Александр <a.bugrimov@inbox.ru>
2016-11-05 00:02:54 +0700

    Изменеия для новой ветки

--- Tests/src/main/java/ru/bugrimov/Run.java
+++ Tests/src/main/java/ru/bugrimov/Run.java
@@ -8,5 +8,6 @@ public class Run {
     public static void main(String[] args) {
         System.out.println("Проверка запуска!!!!");
         System.out.println("Доп. проверка.....");
+        System.out.println("Новое");
     }
 }
