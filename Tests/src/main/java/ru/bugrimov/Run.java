95090e13f56633bd33ae4804365724eec8dd8268
Бугримов Александр <a.bugrimov@inbox.ru>
2016-11-04 22:25:19 +0700

    init

---
+++ Tests/src/main/java/ru/bugrimov/Run.java
@@ -0,0 +1,11 @@
+package ru.bugrimov;
+
+/**
+ * @author Бугримов Александр
+ */
+public class Run {
+
+    public static void main(String[] args) {
+        System.out.println("Проверка запуска");
+    }
+}
